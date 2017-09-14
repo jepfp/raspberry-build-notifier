3d078f9dad7f79124cd8763d03bd49f26af1944d
huj <huj@imis.ch>
2017-09-14 21:32:28 +0200

    daemon vorbereitet

--- src/main/java/ch/loewenfels/raspberrybuildnotifier/App.java
+++ src/main/java/ch/loewenfels/raspberrybuildnotifier/App.java
@@ -1,13 +1,49 @@
 package ch.loewenfels.raspberrybuildnotifier;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import org.apache.commons.daemon.*;
+
+class EchoTask extends TimerTask {
+    @Override
+    public void run() {
+        System.out.println(new Date() + " running ...");
     }
 }
+
+public class App implements Daemon {
+
+    private static Timer timer = null;
+
+    public static void main(String[] args) {
+        timer = new Timer();
+        timer.schedule(new EchoTask(), 0, 1000);
+    }
+
+    @Override
+    public void init(DaemonContext context)
+            throws DaemonInitException, Exception{
+    	System.out.println("init...");
+    }
+
+    @Override
+    public void start() throws Exception {
+        System.out.println("starting ...");
+        main(null);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("stopping ...");
+        if (timer != null) {
+            timer.cancel();
+        }
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("done.");
+    }
+
+ }
