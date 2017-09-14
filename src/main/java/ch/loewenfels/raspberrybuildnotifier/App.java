64adc7d580c5805b4ac9a4d3b984875c6dc88f0f
Philipp Jenni <philippjenni@bluemail.ch>
2017-09-14 20:54:53 +0200

    DEV-0 Initial Commit

---
+++ src/main/java/ch/loewenfels/raspberrybuildnotifier/App.java
@@ -0,0 +1,13 @@
+package ch.loewenfels.raspberrybuildnotifier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
